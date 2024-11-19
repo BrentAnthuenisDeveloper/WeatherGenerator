bb5c81f3d34a28924c4272709cf797783e0b4c72
brent <brent.anthuenis@outlook.com>
2024-11-19 15:58:51 +0100

    icons

--- weatherreport_app/navigation/AboutTabNav.tsx
+++ weatherreport_app/navigation/AboutTabNav.tsx
@@ -1,20 +1,45 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WeatherReports from "../pages/WeatherReports";
-import About from '../pages/About';
-import DetailsStackNav from './DetailsStackNav';
+import About from "../pages/About";
+import DetailsStackNav from "./DetailsStackNav";
+import { Icon } from "react-native-vector-icons/Icon";
 
 const Tab = createBottomTabNavigator();
 
 type MainNavigationParamList = {};
 const AboutTabNav = () => {
 	return (
-		<Tab.Navigator screenOptions={{headerShown:false}}>
-			<Tab.Screen name="about" component={About}
-			 options= {{
-			 	tabBarLabel:'about'
-			 }}
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				headerShown: false,
+				tabBarActiveBackgroundColor: "blue",
+				tabBarActiveTintColor: "calk",
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName: string;
+
+					if (route.name === "about") {
+						iconName = focused ? "Info" : "InfoOutlined";
+					} else if (route.name === "weatherreports") {
+						iconName = focused ? "ViewList" : "ViewListOutlined";
+					}
+					// Return the icon component
+					return <Icon name={iconName} size={size} color={color} />;
+				},
+				tabBarInactiveTintColor: "gray",
+			})}
+		>
+			<Tab.Screen
+				name="about"
+				component={About}
+				options={{
+					tabBarLabel: "about",
+				}}
 			/>
-			<Tab.Screen name="weatherreports" component={DetailsStackNav} options={{tabBarLabel:'weather reports'}}/>
+			<Tab.Screen
+				name="weatherreports"
+				component={DetailsStackNav}
+				options={{ tabBarLabel: "weather reports" }}
+			/>
 		</Tab.Navigator>
 	);
 };
